c63e8c16fab5e1ebc6cd7e7a0f06788ba289d7df
Scott-Arch-Box <scottmhall16@gmail.com>
2021-12-23 08:34:29 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # arch_linux_setup
+
+# Intial setup
+
+First need to partition your disk to allow UEFI setup.  Use **fdisk** to setup the required partitions.  Make sure you use the disk for the machine you are using.
+
+```
+fdisk -l
+```
---
+++ pacman_installs.sh
@@ -0,0 +1,7 @@
+#
+# Basic packages to install via Pacman for Arch Linux initial setup (Must be root)
+#
+
+pacman -Syy
+
+pacman -S linux linux-lts linux-firmware linux-headers linux-lts-headers git nano vim base-devel openssh networkmanager wpa_supplicant wireless_tools netctl dialog lvm2 grub dosfstools mtools efibootmgr os-prober intel-ucode xorg-server virtualbox-guest-utils xf86-video-vmware plasma-meta kde-applications
