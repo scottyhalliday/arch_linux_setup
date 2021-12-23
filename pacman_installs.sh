2a721dc9b27be4aed714eb4ead4d9f8e269d0d67
Scott-Arch-Box <scottmhall16@gmail.com>
2021-12-23 17:34:24 -0500

    Add manual instructions

--- pacman_installs.sh
+++ pacman_installs.sh
@@ -4,4 +4,19 @@
 
 pacman -Syy
 
-pacman -S linux linux-lts linux-firmware linux-headers linux-lts-headers nano vim base-devel openssh networkmanager wpa_supplicant wireless_tools netctl dialog lvm2 grub dosfstools mtools efibootmgr os-prober intel-ucode xorg-server virtualbox-guest-utils xf86-video-vmware plasma-meta kde-applications
+pacman -S linux linux-lts linux-firmware linux-headers linux-lts-headers nano vim base-devel openssh networkmanager wpa_supplicant wireless_tools netctl dialog lvm2 grub dosfstools mtools efibootmgr os-prober intel-ucode xorg-server virtualbox-guest-utils xf86-video-vmware plasma-meta kde-applications man-db
+
+# systemctl enable sshd
+# systemctl enable NetworkManager
+
+# # Create package installation directory
+# mkdir archpackages
+# cd archpackages
+
+# # Install Yay
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+
+# # Install pamac
+# yay -S pamac
